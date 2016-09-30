e34fc22b261649e20a13a6de6bd9f098a13c9769
Andrew James <ajames20@gmail.com>
2016-09-30 21:08:13 +0300

    overlay with image complete

--- practice/flexbox/projects.js
+++ practice/flexbox/projects.js
@@ -1,10 +1,25 @@
-//Add overlay to body
+//variables
 var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
 
+//adds image to overlay
+$overlay.append($image);
+
+//adds overlay ot body
 $("body").append($overlay);
 
+//function to show overlay on click
+
 $("#imageGallery a").click(function(event){
+  //cancels link from opeining as usual
   event.preventDefault();
-  var href = $(this).attr("href");
+  var imgLocation = $(this).attr("href");
+  //update overlay wit image linked location
+  $image.attr("src", imgLocation);
+  //show overlay
   $overlay.show();
+});
+//anon function to hide overlay
+$overlay.click(function(){
+    $overlay.hide();
 });
