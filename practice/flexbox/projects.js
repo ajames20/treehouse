8ce2919e5701da97996d83e44d5327a0adde60ed
Andrew James <ajames20@gmail.com>
2016-09-30 23:08:15 +0300

    nav complete

--- practice/flexbox/projects.js
+++ practice/flexbox/projects.js
@@ -53,6 +53,10 @@ $(".nav a").each(function (){
 
   var $option = $("<option></option>");
 
+  if($anchor.parent().hasClass('selected')) {
+    $option.prop("selected", true);
+  }
+
   $option.val($anchor.attr("href"));
 
   $option.text($anchor.text());
