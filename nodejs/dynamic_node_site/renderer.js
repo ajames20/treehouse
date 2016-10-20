288694d29d1dc289e28e1bddc3016f56337878b7
Andrew James <ajames20@gmail.com>
2016-10-20 15:26:25 +0300

    node dynamic site practice

--- nodejs/dynamic_node_site/renderer.js
+++ nodejs/dynamic_node_site/renderer.js
@@ -1,8 +1,23 @@
+var fs = require("fs");
 
-function view(templatesName, values, response){
-  //read from the template files and
+function mergeValues(values, content) {
+  //Cycle over the keys
+  for(var key in values) {
+    //Replace all {{ key }} with the value from the values object
+    content = content.replace("{{" + key + "}}", values[key]);
+  }
+  //return merged content
+  return content;
+}
+
+function view(templateName, values, response) {
+  //Read from the template file
+  var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
 
   //Insert values into content
-
-  //write out to the response
+  fileContents = mergeValues(values, fileContents);
+  //Write out content to the response
+  response.write(fileContents);
 }
+
+module.exports.view = view;
--- nodejs/dynamic_node_site/router.js
+++ nodejs/dynamic_node_site/router.js
@@ -1,21 +1,27 @@
 var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
+var commonHeader = {
+    'Content-Type': 'text/html'
+};
 
 
 // handle HTTP route GET / and POST / ie Home
 function home(request, response) {
     // if url == "/" && GET
     if (request.url === "/") {
+        if (request.method.toLowerCase() === "get") {
+            //    show search
+            response.writeHead(200, commonHeader);
+            renderer.view("header", {}, response);
+            renderer.view("search", {}, response);
+            renderer.view("footer", {}, response);
+            response.end();
+        } else {
+          
+        }
 
-        //    show search
-        response.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        response.write("Header\n");
-        response.write("Search\n");
-        response.end('Footer\n');
 
-        // if url == "/" && POST
-        //    redirect to /:username
+
     }
 }
 
@@ -24,31 +30,36 @@ function user(request, response) {
     // if url = "/...."
     var username = request.url.replace("/", "");
     if (username.length > 0) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write("Header \n");
+        response.writeHead(200, commonHeader);
+        renderer.view("header", {}, response);
 
         //  get json from Trehouse
         var studentProfile = new Profile(username);
         //on end
         studentProfile.on("end", function(profileJSON) {
-          //show username
-          //store the value we need
-          var values = {
-            avatarUrl: profileJSON.gravatar_url,
-            username: profileJSON.profile_name,
-            badgecount: profileJSON.badges.length,
-            javascriptPoints: profileJSON.points.JavaScript
-          };
-          // show simple message
-          response.write(values.username + " has " + values.badgecount + " badges" + "\n");
-          response.end('Footer\n');
+            //show username
+            //store the value we need
+            var values = {
+                avatarUrl: profileJSON.gravatar_url,
+                username: profileJSON.profile_name,
+                badges: profileJSON.badges.length,
+                JavaScriptPoints: profileJSON.points.JavaScript
+            };
+            // show simple message
+            renderer.view("profile", values, response);
+            renderer.view("footer", {}, response);
+            response.end();
         });
         //    on "error"
         studentProfile.on("error", function(error) {
 
             //      show error
-            response.write(username + "\n");
-            response.end('Footer\n');
+            renderer.view("error", {
+                errorMessage: error.message
+            }, response);
+            renderer.view("search", {}, response);
+            renderer.view("footer", {}, response);
+            response.end();
         });
 
 
