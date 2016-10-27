abfdf45f8475c9129b3f09a1bd49c1a4059a436e
Andrew James <ajames20@gmail.com>
2016-10-27 11:52:50 +0300

    angular custom checkboxes

---
+++ angular/scripts/services/data.js
@@ -0,0 +1,23 @@
+/*jslint node: true*/
+'use strict';
+
+angular.module('todoListApp')
+
+.service('dataService', function($http) {
+
+    this.helloConsole = function() {
+        console.log('This is the helloConsole function');
+    };
+    this.getTodos = function(callback) {
+        $http.get('mock/todos.json')
+          .then(callback);
+    };
+
+    this.deleteTodo = function(todo){
+      console.log("The " + todo.name + " has been deleted!");
+    };
+
+    this.saveTodo = function(todo){
+      console.log("The " + todo.name + " has been saved!");
+    };
+});
